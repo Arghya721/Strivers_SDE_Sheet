cf32b38ed50121885a6f3ad3dc28e429fb00a3fe
Arghyadeep Ghosh <60299552+Arghya721@users.noreply.github.com>
2023-06-07 19:23:15 +0000

    Added Arrays-II

---
+++ Arrays-II/mergeInterval/mergeInterval.go
@@ -0,0 +1,59 @@
+/* Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+Example 1:
+
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+Example 2:
+
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+
+Constraints:
+
+1 <= intervals.length <= 10^4
+intervals[i].length == 2
+0 <= starti <= endi <= 10^4
+
+*/
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func merge(intervals [][]int) [][]int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	merged := [][]int{}
+
+	for i := 0; i < len(intervals); i++ {
+		if len(merged) == 0 || merged[len(merged)-1][1] < intervals[i][0] {
+			temp := []int{intervals[i][0], intervals[i][1]}
+			merged = append(merged, temp)
+		} else {
+			merged[len(merged)-1][1] = max(merged[len(merged)-1][1], intervals[i][1])
+		}
+	}
+
+	return merged
+}
+
+func main() {
+	intervals := [][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}}
+	fmt.Println(merge(intervals))
+}
